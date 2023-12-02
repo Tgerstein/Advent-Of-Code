cee7571525caa5ff7755e8f99e7f556dd8176aca
Flaris1999 <tim.g35@gso.schule.koeln>
2023-12-02 15:07:45 +0100

    Test

--- Tim/Day_1/obj/Debug/net8.0/Day_1.AssemblyInfo.cs
+++ Tim/Day_1/obj/Debug/net8.0/Day_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca26c3823ec8999ff6c6173e82805d7cea146749")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
